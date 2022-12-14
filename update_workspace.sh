d59da04af2c84b94367cb1e281988dbbbad4f11f
ArneDuyver <arne_duyver@hotmail.com>
2022-12-14 16:48:57 +0000

    update_workspace_sh__added_force_helppyscripts

--- update_workspace.sh
+++ update_workspace.sh
@@ -8,6 +8,15 @@ rm $EXDIR/update_workspace.sh
 
 rm $EXDIR/modeleerFuncties.py
 cp $GITDIR/modeleerFuncties.py $EXDIR/modeleerFuncties.py
+rm $EXDIR/knipFuncties.py
+cp $GITDIR/knipFuncties.py $EXDIR/knipFuncties.py
+rm $EXDIR/plotFuncties.py
+cp $GITDIR/plotFuncties.py $EXDIR/plotFuncties.py
+
+rm $EXDIR/hulpfunctiesBroeikasgassen.py
+cp $GITDIR/hulpfunctiesBroeikasgassen.py $EXDIR/hulpfunctiesBroeikasgassen.py
+rm $EXDIR/hulpfunctiesKlimaatopwarming.py
+cp $GITDIR/hulpfunctiesKlimaatopwarming.py $EXDIR/hulpfunctiesKlimaatopwarming.py
 
 chmod -w $EXDIR/hulpfunctiesBroeikasgassen.py 
 chmod -w $EXDIR/hulpfunctiesKlimaatopwarming.py 
