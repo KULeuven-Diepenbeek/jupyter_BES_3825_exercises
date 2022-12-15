cfa4ee27c3a9f380e70cf5adc0a4fe596e82fe95
ArneDuyver <arne_duyver@hotmail.com>
2022-12-15 13:35:46 +0000

    update_projectielbeweging_versienr

--- update_workspace.sh
+++ update_workspace.sh
@@ -36,7 +36,7 @@ cp -r $GITDIR/assets $EXDIR/assets
 
 echo "Bekijk hieronder de lijst met geupdate bestanden. Reset de bestanden die je wenst te vernieuwen."
 # echo -e "\033[46;30m - Geen nieuwe versies van bestanden "
-# echo -e "\033[46;30m - K2W1-Projectielbeweging.ipynb kreeg een nieuwere versie "
+# echo -e "\033[46;30m - K2W1-Projectielbeweging.ipynb kreeg een nieuwere versie: Versie 1.0 "
 echo "\033[46;30m - K2W3-Klimaatopwarming.ipynb kreeg een nieuwere versie: Versie 1.1 "
 
 echo "\033[0mDONE"
